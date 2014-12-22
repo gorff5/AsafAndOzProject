f156a30461879c957ba0406c637a3baa4bd5c537
asaf-laptop <hellooo>
2014-12-22 18:26:29 +0200

    we game is not

---
+++ Messanger_HttpAsyncHandler_ASP_Mobile/App_Code/game_code/game.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// this is the game class, for every instance we create game with players,ques,scores etc.
+/// </summary>
+public class game
+{
+	public game()
+	{
+		//
+		// hi
+		//
+	}
+}
